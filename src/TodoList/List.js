8d688a030cf8e12f5d6a8d2f7b6d3a28b0758cb0
RicardoKd <richardo.kadyan@gmail.com>
2020-06-17 20:11:07 +0300

    First commit

---
+++ src/TodoList/List.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const List = () => {
+  return <div>It is a list</div>;
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export { List };
---
+++ src/TodoList/Task.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Task = () => {
+  return <p>It is a task item</p>;
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export { Task };
